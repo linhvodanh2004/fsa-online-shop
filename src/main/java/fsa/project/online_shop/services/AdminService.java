6840b4f12559a880fc092591c59ea56fada29889
miit06 <ngthdat10@gmail.com>
2025-07-17 09:30:06 +0700

    Config controller, repo, service for admin

--- src/main/java/fsa/project/online_shop/services/AdminService.java
+++
@@ -1,9 +0,0 @@
-package fsa.project.online_shop.services;
-
-import java.util.List;
-
-import fsa.project.online_shop.models.Product;
-
-public interface AdminService {
-    public List<Product> getAllProducts();
-}
